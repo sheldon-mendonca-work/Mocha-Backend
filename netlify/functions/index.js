b696eca31d1a35695017b1df7b3fec6725d224e6
sheldomaniac <41835700+sheldomaniac@users.noreply.github.com>
2023-07-03 00:03:18 +0530

    Update index.js

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -30,16 +30,9 @@ db.once('open', ()=>{
 const app = express();
 const port = 3001;
 /* Express Routing */
-const router = Router();
 app.use(cors());
-app.options('*',cors());
-var allowCrossDomain = function(req,res,next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();  
-}
-app.use(allowCrossDomain);
+const router = Router();
+
 
 // app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());

9af3485848036af2b50241647dde857fc121ea8f
sheldomaniac <41835700+sheldomaniac@users.noreply.github.com>
2023-07-03 00:36:55 +0530

    Update index.js

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -31,7 +31,7 @@ const app = express();
 const port = 3001;
 /* Express Routing */
 const allowCrossDomain = (req, res, next) => {
-    res.header(`Access-Control-Allow-Origin`, `example.com`);
+    res.header(`Access-Control-Allow-Origin`, `http://localhost:3000`);
     res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
     res.header(`Access-Control-Allow-Headers`, `Content-Type`);
     next();

15c4f1acebe7cb9dc7f83d86aac8aa89c7d13c10
sheldomaniac <41835700+sheldomaniac@users.noreply.github.com>
2023-07-04 00:03:12 +0530

    Update seeds.js

--- src/seeds/seeds.js
+++ src/seeds/seeds.js
@@ -30,43 +30,15 @@ const userSeedDB = async() => {
         phNo: "4281321649",
         dateOfBirth: "1996-12-10",
         bio: "A test user checking out this app",
-        profileImg: "1dbce2d2-b7f6-4150-9539-b3be3f2a6ee3", 
-        posts: ["1c55ec33-9694-4d6e-8d9a-105dd15af267"],
         encodedToken: encodedToken
       });
 
     await user1.save();
 
-    const post1 = new Post({ //
-      _id: "1c55ec33-9694-4d6e-8d9a-105dd15af267",
-      postDec:
-      `🏆 Premier League winner
-🏆 Premier League Golden Boot winner
-🏆 Premier League Player of the Season
-🏆 FA Cup winner
-🏆 Champions League winner
     
-Erling Haaland's first season at Man City was… not bad. 😮‍💨🧘🏼‍♂️`,
-      postImgLink: ["cf1e7edd-f78c-4773-bcef-f146e52f80ea"],
-      parentPost: null,
-      user_id: user1._id,
-      
-    });
-  
-    await post1.save();
 
 }
 
 userSeedDB().then(() => {
     mongoose.connection.close();
 })
-
-// const postSeedDB = async() => {
-
-  
-
-// }
-
-// postSeedDB().then(() => {
-//   mongoose.connection.close();
-// })
